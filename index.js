c67a8503eb8c964c9f870463f17e34d512475616
Gabriel Caetano <127317414+GcsNJ@users.noreply.github.com>
2023-08-25 14:10:41 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+
+// // //Função para digitar comandos 
+// // let produto =(prompt('1.Adicionar Itens\n2.Remover Itens\n3.Ver Estoque\n4.Sair'));
+
+// // //Função para exibir a lista de itens no estoque
+// // function  exibirEstoque(){
+// //     if(exibirEstoque.length === 0) {
+// //         alert("O estoque está vazio.");
+// //     return;
+// // }
+// // var listaItens = "Itens no estoque: \n";
+// // exibirEstoque.ForEach (function(item, index){
+// //     listaItens += index + ": " +item + "\n";
+// //     });
+// //     alert(listaItens);
+// // }
+
+// let estoque= [];
+
+// function adicionarItem(){
+//     let novoItem = prompt('Digite o nome do novo Item:');
+//     estoque.push(novoItem);
+//     alert('Item adcionadoao estoque: ' + novoItem);
+// }
+
+// function removerItem(){
+//     if (estoque.length === 0){
+//         alert('O estoque está vazio.');
+//         return;
+//     }
+//     let itemRemover = prompt('Digite o índice do item a ser removido (0 até ')
+// }
+
+
+
+// //Loop para executar o progama até que o usuário decida sair 
+// while(true){
+// let opção =(prompt('1.Adicionar Itens\n2.Remover Itens\n3.Ver Estoque\n4.Sair'));
+
+// switch (opção){
+//     case"1"
+//     adicionarItem()
+// }}
